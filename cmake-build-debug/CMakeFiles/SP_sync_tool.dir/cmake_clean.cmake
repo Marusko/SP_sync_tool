1e06a50227c20041c618ea448c07b9bd6bdd036f
Matúš Suský <matussusky@gmail.com>
2023-12-23 13:33:07 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/SP_sync_tool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SP_sync_tool.dir/main.cpp.o"
+  "SP_sync_tool"
+  "SP_sync_tool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SP_sync_tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/SP_sync_tool.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SP_sync_tool.
+# This may be replaced when dependencies are built.
