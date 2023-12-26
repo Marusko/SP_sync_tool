3c3aabd5fb1ffc97b1b82aac91dd22b75bfe5740
kucera <kucera.david2002@gmail.com>
2023-12-26 16:50:05 +0100

    prvotne rozdelenie aplikacie na klient a server cast

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        std::cout << "You need to specify a program argument -client or -server!" << std::endl;
+        return 1;
+    }
+
+    std::string argument = argv[1];
+
+    if (argument == "-client") {
+        std::cout << "Program is running as client." << std::endl;
+        // TODO start client
+    } else if (argument == "-server") {
+        std::cout << "Program is running as server." << std::endl;
+        // TODO start server
+    } else {
+        std::cout << "Invalid startup argument! Use -client or -server!" << std::endl;
+    }
+
     return 0;
 }
